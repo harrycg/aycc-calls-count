5e89db7b941ae31e96ceab753e67e9e9fe00f55d
harrycg <39111936+harrycg@users.noreply.github.com>
2018-12-12 10:01:41 +0930

    Update test.rb

--- test.rb
+++ test.rb
@@ -2,7 +2,7 @@
 
 require 'nationbuilder'
 
-client = NationBuilder::Client.new('harrycossar', ENV['NATIONBUILDER_APIKEY'], retries: 8)
+client = NationBuilder::Client.new('aycc', ENV['NATIONBUILDER_APIKEY'], retries: 8)
 
 puts "finding wants to vols"
 
